e7cbd058571fbbbf7b2f8bc6f7a77b512da8b4e9
SVolowy4 <aqualoongrap@gmail.com>
2024-11-05 21:54:23 +0200

    made cartQuantity with DOM to page

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -74,8 +74,16 @@ document.querySelectorAll('.js-add-to-cart-button').forEach((btn)=>{
         quantity: 1
       })
     }
+    
+    let cartQuantity = 0
+    cart.forEach(item => {
+      cartQuantity += item.quantity
+    })
+    
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity
 
     console.log(cart)
+    console.log(cartQuantity)
   })
 })
 
